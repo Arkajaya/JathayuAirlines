0f3bc986cb49d60b4f6fb6c91a17707d0be7cce8
Arkajaya <hendraharyono0@gmail.com>
2025-12-30 00:50:16 +0700

    atur seed + factory user, service

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Create roles (using Spatie Permission fields)
         $roles = [
             ['name' => 'Admin', 'guard_name' => 'web'],
             ['name' => 'Staff', 'guard_name' => 'web'],
@@ -43,36 +42,29 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
 
-        // Assign Spatie roles to created users to keep role checks consistent
+        // Assign Spatie roles 
         try {
             $admin->assignRole('Admin');
         } catch (\Exception $e) {
-            // ignore if roles not configured
+            // abaikan
         }
 
         try {
             $staff->assignRole('Staff');
         } catch (\Exception $e) {
-            // ignore if roles not configured
+            // abaikan
         }
 
-        // Create additional regular users
+        // adding regular users
         $additionalUsers = User::factory(30)->create()->each(function ($u) {
-            $u->role_id = 3; // regular user role_id
+            $u->role_id = 3; 
             $u->save();
             try { $u->assignRole('User'); } catch (\Exception $e) { }
         });
 
-        // Create services (flights)
+        // services (flights)
         Service::factory(20)->create();
 
-        // Create blogs using factory, set featured_image to Unsplash random images (travel themed)
-        for ($i = 1; $i <= 12; $i++) {
-            $blog = Blog::factory()->make();
-            // Use Unsplash random image with a signature to vary images
-            $blog->featured_image = 'https://source.unsplash.com/random/1200x800?travel,airplane&sig=' . $i;
-            $blog->save();
-        }
 
     }
 }
